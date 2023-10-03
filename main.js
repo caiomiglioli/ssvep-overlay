3c1580cb0a7e71f1b5570ec8793bc66ce11101a9
caiomiglioli <caiomiglioli@gmail.com>
2023-10-03 19:30:56 -0300

    start

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const createWindow = require('./src/pages/index')
+
+//auto reload
+const env = process.env.NODE_ENV || 'development';
+  
+// If development environment
+if (env === 'development') {
+    try {
+        require('electron-reloader')(module, {
+            debug: true,
+            watchRenderer: true,
+            hardResetMethod: 'exit'
+        });
+    } catch (_) { console.log('Error'); }    
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
